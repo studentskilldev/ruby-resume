d73e05d8b090bfddae9a3249fb9f9ea8d21ded53
Rodolfo Peixoto <rodolfo.peixoto@rogpe.tech>
2023-02-06 12:52:16 -0300

    Add file hello_world and if_else.rb

---
+++ if_else.rb
@@ -0,0 +1,15 @@
+# if condition
+#     # code to execute if condition is true
+# else
+#     # code to exdecute if is false
+# end
+
+x = 10
+
+if x > 15
+    puts 'x é maior que 15'
+elsif x > 5
+    puts 'X é maior que 5 e menor ou igual a 15'
+else
+    puts 'x é menor ou igual a 5'
+end
