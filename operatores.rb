b92423ef1f57ec71142ca88d30c060f7dde8aa35
Rodolfo Peixoto <rodolfo.peixoto@rogpe.tech>
2023-02-06 07:39:10 -0300

    Add file about operatores

---
+++ operatores.rb
@@ -0,0 +1,17 @@
+# Operadores:
+# Ruby suporta vários operadores, incluindo aritméticos 
+# (+, -, *, /), de comparação (==, !=, >, <, >=, <=) e lógicos (&&, ||, !).
+
+x = 5
+y = 3
+puts x + y # Output: 8
+puts x * y # Output: 15
+puts x == y # Output: false
+puts x != y # Output: true
+puts x > y # Output: true
+puts x < y # Output: false
+puts x >= y # Output: true
+puts x <= y # Output: false
+puts ''
+puts (x > y) && (y > x)  # Output: false
+puts x > y || y > x  # Output: true
