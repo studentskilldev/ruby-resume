a0b43f5c16fbbb0ab4defabd98054989fd7982fa
Rodolfo Peixoto <rodolfo.peixoto@rogpe.tech>
2023-02-06 07:27:11 -0300

    Add data type information

---
+++ data_type.rb
@@ -0,0 +1,13 @@
+# * String: Uma sequência de caracteres, que deve ser delimitada por aspas duplas ou as simples
+# * Inteiro: Númerios intereiros, sem parte fracionária.
+# * Float: Número de ponto flutuante, com uma parte fracionária. 
+# * Array: Coleção ordenada de objetos
+# * Hash: Conjunto não ordenado de chaves e valores
+
+
+string = "Olá, eu sou o Rodolfo Peixoto"
+string2 = 'Olá, eu sou o Rodolfo Peixoto'
+integer = 2
+float = 3.14
+array = [1,2,3,4,5]
+hash = { name: 'Rodolfo', age: 32 }
