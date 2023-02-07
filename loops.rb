e4d8a628a3ea3c0108b6e4d98f08701665f4b15b
Rodolfo Peixoto <rodolfo.peixoto@rogpe.tech>
2023-02-07 12:45:53 -0300

    Add until, while, do while and each context

--- loops.rb
+++ loops.rb
@@ -4,27 +4,33 @@
 #   # code
 # end
 
-# numbers = [1,2, 3, 4, 5]
-# amount = numbers.length
+numbers = [1,2, 3, 4, 5]
+amount = numbers.length
 
-# for number in numbers
-#   puts "number #{number * 2}"
-# end
+for number in numbers
+  puts "number #{number * 2}"
+end
 
-# numbers.each_with_index do |number, position|
-#   puts "Position #{position} | Number: #{number * numbers[4]}"
-#   puts ''
-#   puts "numbers[position]: #{numbers[position]}}"
-# end
+numbers.each_with_index do |number, position|
+  puts "Position #{position} | Number: #{number * numbers[4]}"
+  puts ''
+  puts "numbers[position]: #{numbers[position]}}"
+end
 
-counter = 10
+counter = 1
 
-# while counter <= 6
-#   puts counter
-#   counter += 1
-# end
+while counter <= 6
+  puts counter
+  counter += 1
+end
 
 begin
   puts counter
   counter += 1
 end while counter <= 5
+
+
+until counter > 5 # false
+  puts counter
+  counter += 1
+end
