68319ce3712ac1aa5a36f561171102138c9a8eec
Rodolfo Peixoto <rodolfo.peixoto@rogpe.tech>
2023-02-07 12:27:46 -0300

    Add files case.rb and loops.rb

---
+++ case.rb
@@ -0,0 +1,23 @@
+# case expression
+# when value1
+#     # code
+# when value2
+#     # code
+# when value3
+#     # code
+# else
+#     # code
+# end
+
+x = 50
+
+case x
+when 0
+  puts 'x é 0'
+when 5
+  puts 'x é 5'
+when 10
+  puts 'x é 10'
+else
+  puts 'x é outro número'
+end
---
+++ loops.rb
@@ -0,0 +1,30 @@
+# For -> para um número conhecido de vezes
+
+# for variable in start_value..end_value
+#   # code
+# end
+
+# numbers = [1,2, 3, 4, 5]
+# amount = numbers.length
+
+# for number in numbers
+#   puts "number #{number * 2}"
+# end
+
+# numbers.each_with_index do |number, position|
+#   puts "Position #{position} | Number: #{number * numbers[4]}"
+#   puts ''
+#   puts "numbers[position]: #{numbers[position]}}"
+# end
+
+counter = 10
+
+# while counter <= 6
+#   puts counter
+#   counter += 1
+# end
+
+begin
+  puts counter
+  counter += 1
+end while counter <= 5
